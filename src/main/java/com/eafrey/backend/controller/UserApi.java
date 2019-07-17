c7a2028f824b2c662cedc6dcbabe8c9c6a23ea33
Sen Chen <senchen@thoughtworks.com>
2019-07-17 23:27:33 +0800

    test pipeline

--- src/main/java/com/eafrey/backend/controller/UserApi.java
+++ src/main/java/com/eafrey/backend/controller/UserApi.java
@@ -2,24 +2,23 @@ package com.eafrey.backend.controller;
 
 import com.eafrey.backend.model.User;
 import com.eafrey.backend.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class UserApi {
     @Autowired
     private UserService userService;
 
-    private Logger logger = LoggerFactory.getLogger(UserApi.class);
-
     @PostMapping("/api/user")
     public ResponseEntity createUser(@RequestBody User user) {
-        logger.info("request add user {}", user);
-        return ResponseEntity.ok(userService.save(user));
+        log.info("request add user {}", user);
+//        userService.save(user);
+        return ResponseEntity.ok("");
     }
 }
