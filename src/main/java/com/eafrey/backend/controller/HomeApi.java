7fe098a0fad69601f513f62dfdb8459ac074102d
Sen Chen <senchen@thoughtworks.com>
2019-08-10 22:33:21 +0800

    add servlet.context-path=/api

--- src/main/java/com/eafrey/backend/controller/HomeApi.java
+++ src/main/java/com/eafrey/backend/controller/HomeApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeApi {
 
-    @GetMapping("/api/test")
+    @GetMapping("/test")
     public String getTest() {
         return "test-eafrey-cn";
     }
